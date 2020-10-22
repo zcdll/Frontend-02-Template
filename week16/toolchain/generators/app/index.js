ea71b689f3713cfa2658e8370974804f8a547c69
zcdll <qinsword521@gmail.com>
2020-10-23 07:51:42 +0800

    week16 finish 02

--- week16/toolchain/generators/app/index.js
+++ week16/toolchain/generators/app/index.js
@@ -9,11 +9,46 @@ module.exports = class extends Generator {
     // this.option("babel"); // This method adds support for a `--babel` flag
   }
 
-  method1() {
-    this.log("method 1 just ran");
+  initPackage() {
+    const pkgJson = {
+      devDependencies: {
+        eslint: "^3.15.0",
+      },
+      dependencies: {
+        react: "^16.2.0",
+      },
+    };
+
+    // Extend or create package.json file in destination path
+    this.fs.extendJSON(this.destinationPath("package.json"), pkgJson);
+
+    this.npmInstall();
+  }
+
+  async method1() {
+    const answers = await this.prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Your project name",
+        default: this.appname, // Default to current folder name
+      },
+      {
+        type: "confirm",
+        name: "cool",
+        message: "Would you like to enable the Cool feature?",
+      },
+    ]);
+
+    this.log("app name", answers.name);
+    this.log("cool feature", answers.cool);
   }
 
   method2() {
-    this.log("method 2 just ran");
+    this.fs.copyTpl(
+      this.templatePath("t.html"),
+      this.destinationPath("public/index.html"),
+      { title: "Templating with Yeoman" }
+    );
   }
 };
