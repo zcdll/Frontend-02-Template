ef1e0c38a5045eb20a383727e8a4e993485a00f6
zcdll <qinsword521@gmail.com>
2020-10-25 22:30:22 +0800

    week16 finish 03

---
+++ week16/toolchain/generator-vue/generators/app/templates/webpack.config.js
@@ -0,0 +1,32 @@
+const webpack = require("webpack");
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+const path = require("path");
+
+const a = `${__dirname}/src/main.js`;
+
+console.log(a, "---a");
+
+module.exports = {
+  entry: path.resolve(`${__dirname}/src/main.js`),
+  // output: {
+  //   path: path.resolve(__dirname, "dist"),
+  //   filename: "my-first-webpack.bundle.js",
+  // },
+  module: {
+    rules: [
+      { test: /\.vue$/, use: "vue-loader" },
+      {
+        test: /\.css$/,
+        use: ["vue-style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new CopyPlugin({
+      patterns: [{ from: "src/*.html", to: "[name].[ext]" }],
+    }),
+  ],
+};
